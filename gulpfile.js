a3560dc3f3651561e1f8ae714f9cd836966a05ef
BradBroulik <brad.broulik@gmail.com>
2014-03-15 16:57:43 -0500

    Added package.json for gulp dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,27 @@
+/* 
+ * Run Gulp: gulp
+ * Install npm dependencias: npm -install 
+ */
 var gulp = require('gulp'),
 	htmlhint = require("gulp-htmlhint"),
 	csslint = require('gulp-csslint'),
 	jshint = require('gulp-jshint');
 
-
+/* Run via: gulp */
 gulp.task('default', function() {
 	gulp.src(['./**/*.html', '!./**/includes/*.html'])
  		.pipe(htmlhint())
   		.pipe(htmlhint.reporter());
 });
 
+/* Run via: gulp css */
 gulp.task('css', function() {
   	gulp.src('./**/*.css')
     	.pipe(csslint())
     	.pipe(csslint.reporter());
 });
 
+/* Run via: gulp javascript */
 gulp.task('javascript', function() {
   gulp.src('./**/*.js')
     .pipe(jshint())
